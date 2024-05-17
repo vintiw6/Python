3c0e74a115df7e492ab0d398e5ccfd7f7d0efeee
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2024-05-17 17:21:14 +0530

    Update docstrings.py

---
+++ Basics/docstrings.py
@@ -0,0 +1,10 @@
+def square(n):
+    '''Takes in a number n , returns the square of n'''
+    print(n**2)    
+square(2)
+
+# Doc-Strings is similar to comment but it a String which is used to tell us about the function
+# its is used just after the function definition
+
+print(square.__doc__)
+# Used to Access Doc-String
